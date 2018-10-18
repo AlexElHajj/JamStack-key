babd355f5e84d66440c908c1349f57ae46d0bf9a
Alex <elhajjalex@gmail.com>
2018-10-17 19:16:55 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>It's time to build something great!</p>
+
+  </Layout>
+)
+
+export default IndexPage
